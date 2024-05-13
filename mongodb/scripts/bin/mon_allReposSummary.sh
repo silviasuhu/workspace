4f9548c9c647c20302d2c2ec341b9ab454e044d6
Silvia Surroca <silvia.surroca@mongodb.com>
2024-05-13 12:59:49 +0000

    Improve mon_allReposSummary.sh script

--- mongodb/scripts/bin/mon_allReposSummary.sh
+++ mongodb/scripts/bin/mon_allReposSummary.sh
@@ -19,27 +19,55 @@ for dir in $(find $ROOT_PATH -maxdepth 1 -type d -print0 | sort -z | xargs -0); 
         
         # Get current branch
         branch=$(git rev-parse --abbrev-ref HEAD)
-        
-        # Check if that's the current branch
-        highligh="${normal}"
-        if [[ ! -z "$currentBranch" ]]; then
-            if [[ "$branch" == "$currentBranch" ]]; then
-                highligh="${bold}*"
+
+        gitDescribe=$(git describe --abbrev=0 HEAD)
+
+        # Get the base branch
+        if [[ "$gitDescribe" == *"alpha"* ]]; then
+            baseBranch="master"
+        else
+            if [[ $gitDescribe =~ ^r([0-9]+)\.([0-9]+)\. ]]; then
+                major=${BASH_REMATCH[1]}
+                minor=${BASH_REMATCH[2]}
+            else
+                echo "Invalid version format: $gitDescribe got from branch: $branch"
+                exit 1
             fi
+            baseBranch="v$major.$minor"
+        fi
+
+        # Get the base commit
+        baseCommit=$(git merge-base $baseBranch HEAD)
+        baseCommitDate=$(git log --format=%ad --date=short -1 $baseCommit)
+        branchPointingBaseCommit=$(git branch --points-at $baseCommit)
+
+        baseCommitSummary="$baseCommitDate, ${baseCommit:0:5}"
+        
+        # Hightligh the line if it's the current branch
+        if [[ ! -z "$currentBranch" ]] && [[ "$branch" == "$currentBranch" ]]; then
+            highlightStart="\e[32;1m* "
+            highlightEnd="\e[32;0m"
+            first_column_width=14
+        else
+            highlightStart=""
+            highlightEnd=""
+            first_column_width=16
         fi
 
         # Extract the JIRA ticket from the branch name
         jira_ticket=$(echo "$branch" | awk -F/ '{print $NF}')
         
-        # Get the jira ticket information
+        # Get the jira ticket summary
         response=$(curl -s "$JIRA_URL/rest/api/2/issue/$jira_ticket")
         summary=$(echo "$response" | jq -r '.fields.summary') 
 
+
         if [ -z "$summary" ] || [ "$summary" == "null" ]; then
-            echo "$highligh $dirName : $branch"
+            # echo -e "$highligh $dirName \t\t $branch"
+            printf "${highlightStart}%-${first_column_width}s [%s] %s${highlightEnd}\n" "$dirName" "$baseCommitSummary" "$branch"
         else
             status=$(echo "$response" | jq -r '.fields.status.name')
-            echo "$highligh $dirName : $jira_ticket ($status), $summary"
+            printf "${highlightStart}%-${first_column_width}s [%s] %s${highlightEnd}\n" "$dirName" "$baseCommitSummary" "$jira_ticket ($status), $summary"
         fi
         popd > /dev/null
     fi
